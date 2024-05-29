d826699d1544f8bdf18bae8c408eede8f97f1787
Tahmid Ahmed <tahmid@midhat.io>
2024-05-29 10:12:14 -0700

    rpa

--- client/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-// TODO: - Tidy up email and password guidelines and align with server
-//       - Transitions on routes
-//       - Dashboard routes to be created (figure out the design flow)
-
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
